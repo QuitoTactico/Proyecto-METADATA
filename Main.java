88319e62e87f1a59f5549425280ee3d8020d86db
Esteban Vergara Giraldo <Teteban0917@gmail.com>
2022-06-01 03:56:35 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@ public static void Contadorcito(){
     String ficheritos = "Ficherito.txt";
     try (Stream<String> stream = Files.lines(Paths.get(ficheritos))) {
         Stream.of(nombre)
-            .filter(element -> element.equals("Buscar"))
+            .filter(element -> element.equals("color"))
             .forEach(System.out::println);
     } catch (IOException e) {
         e.printStackTrace();
@@ -52,11 +52,11 @@ public static void mostrarMenu(){
         System.out.println("#2 para consultar las por la palabra (chiguiro) .");
         System.out.println("#3 para consultar por el texto (Grand Theft Auto).");
         Scanner sca = new Scanner(System.in);
-        int opcion =sca.nextInt();
+        int opcion = sca.nextInt();
         switch(opcion){
             case 0 :
-                loop=0;
-                break;                   
+                loop = 0;
+                break;                  
             case 1:
                 System.out.println("Ha escogido la opcion año 2004");
                 link="https://www.bing.com/search?q=2004&qs=n&form=QBRE&sp=-1&pq=2&sc=6-1&sk=&cvid=39D8A7894BA74E8792F967B21D9038D0";
@@ -78,31 +78,31 @@ public static void mostrarMenu(){
                     }
                 }
             case 2:
-            System.out.println("Ha escogido la opcion palabra chiguiro");
-            link="https://www.bing.com/search?q=chiguiro&form=QBLH&sp=-1&pq=chigui&sc=6-6&qs=n&sk=&cvid=D189E7CC8FA74660BEAB2A5A81CE7BF4";
-            nombre="Chiguiro.txt";
+                System.out.println("Ha escogido la opcion palabra chiguiro");
+                link="https://www.bing.com/search?q=chiguiro&form=QBLH&sp=-1&pq=chigui&sc=6-6&qs=n&sk=&cvid=D189E7CC8FA74660BEAB2A5A81CE7BF4";
+                nombre="Chiguiro.txt";
+                    getMetadata();
+                    Contadorcito();
+                    boolean ve=true;
+                    while(ve==true){
+                        System.out.println("Escriba 0 para volver al menú");
+                        int regresar=sca.nextInt();
+                        if(regresar==0){
+                            System.out.println("Ha regresado al menu");
+                            ver=false;
+                            mostrarMenu();
+                        }else {
+                            ver=false;
+                            System.out.println("Ha regresado al menu");
+                            mostrarMenu();
+                        }
+                    }
+            case 3:
+                System.out.println("Ha escogido la opcion texto Grand Theft Auto");
+                link="https://www.bing.com/search?q=Grand+Theft+Auto&qs=n&form=QBRE&sp=-1&pq=grand+theft+auto&sc=6-16&sk=&cvid=A82F77F7F7DC467D8E14A79431FBFF67";
+                nombre="GTA.txt";
                 getMetadata();
                 Contadorcito();
-                boolean ve=true;
-                while(ve==true){
-                    System.out.println("Escriba 0 para volver al menú");
-                    int regresar=sca.nextInt();
-                    if(regresar==0){
-                        System.out.println("Ha regresado al menu");
-                        ver=false;
-                        mostrarMenu();
-                    }else {
-                        ver=false;
-                        System.out.println("Ha regresado al menu");
-                        mostrarMenu();
-                    }
-                }
-                case 3:
-                System.out.println("Ha escogido la opcion texto Grand Theft Auto");
-            link="https://www.bing.com/search?q=Grand+Theft+Auto&qs=n&form=QBRE&sp=-1&pq=grand+theft+auto&sc=6-16&sk=&cvid=A82F77F7F7DC467D8E14A79431FBFF67";
-            nombre="GTA.txt";
-            getMetadata();
-            Contadorcito();
                 boolean vr=true;
                 while(vr==true){
                     System.out.println("Escriba 0 para volver al menú");
@@ -118,6 +118,7 @@ public static void mostrarMenu(){
                     }
                 }
             }
+            sca.close();
             break;
         }
     }
